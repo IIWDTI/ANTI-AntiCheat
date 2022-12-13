7d2f1290b423626e61ba2d037a29bd66dff85156
IIWDTI <60497271+IIWDTI@users.noreply.github.com>
2022-12-13 22:20:50 +0100

    Add files via upload

---
+++ ANTI-AntiCheat.bat
@@ -0,0 +1,71 @@
+sc stop vgk
+sc stop vgc
+sc stop BEService
+sc stop xhunter1
+sc stop EasyAntiCheat_EOS
+net stop vgk
+net stop vgc
+net stop BEService
+net stop xhunter1
+net stop EasyAntiCheat_EOS
+TASKKILL /F /IM installer.exe /T
+TASKKILL /F /IM log-uploader.exe /T
+TASKKILL /F /IM vgc.exe /T
+TASKKILL /F /IM vgtray.exe /T
+TASKKILL /F /IM BEService.exe /T
+TASKKILL /F /IM BEService_pubg.exe /T
+TASKKILL /F /IM uninstaller.exe /T
+TASKKILL /F /IM zksvc.exe /T
+TASKKILL /F /IM ucldr_battlegrounds_gl.exe /T
+TASKKILL /F /IM ExecPubg.exe /T
+TASKKILL /F /IM TslGame.exe /T
+TASKKILL /F /IM TslGame_BE.exe /T
+TASKKILL /F /IM TslGame_UC.exe /T
+TASKKILL /F /IM TslGame_ZK.exe /T
+TASKKILL /F /IM ACE-Launcher64.exe /T
+TASKKILL /F /IM BEService_x64.exe /T
+TASKKILL /F /IM RiotClientServices.exe /T
+TASKKILL /F /IM VALORANT.exe /T
+TASKKILL /F /IM EasyAntiCheat_EOS.exe /T
+TASKKILL /F /IM cod.exe /T
+TASKKILL /F /IM sp22-cod.exe /T
+TASKKILL /F /IM rust.exe /T
+
+REG DELETE "HKLM\SOFTWARE\Classes\.CETRAINER" /f
+REG DELETE "HKLM\SOFTWARE\Classes\.CT" /f
+REG DELETE "HKLM\SOFTWARE\Classes\CheatEngine" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Cheat Engine_is1" /f
+REG DELETE "HKCU\Software\Cheat Engine" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\ucldr_battlegrounds_gl" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\xhunter1" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\zksvc" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\BEService" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\ucldr_battlegrounds_gl" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\xhunter1" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\zksvc" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Riot Vanguard" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\vgc" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\vgk" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\vgc" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\vgk" /f
+REG DELETE "HKLM\SOFTWARE\vgk" /f
+REG DELETE "HKLM\SOFTWARE\WOW6432Node\EasyAntiCheat_EOS" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\EasyAntiCheat_EOS" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\EasyAntiCheat_EOS" /f
+REG DELETE "HKLM\SYSTEM\ControlSet001\Services\atvi-randgrid_sr" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\atvi-randgrid_sr" /f
+
+rmdir /S /Q "C:\Program Files (x86)\EasyAntiCheat_EOS"
+rmdir /S /Q "C:\Program Files\Riot Vanguard"
+rmdir /S /Q "C:\Program Files (x86)\Common Files\BattlEye"
+rmdir /S /Q "C:\Program Files\Common Files\PUBG"
+rmdir /S /Q "C:\Program Files\Common Files\Wellbia.com"
+rmdir /S /Q "C:\Users\%username%\AppData\Local\WELLBIA"
+rmdir /S /Q "C:\Users\%username%\AppData\LocalLow\Facepunch Studios LTD"
+rmdir /S /Q "C:\Users\%username%\AppData\Roaming\EasyAntiCheat"
+
+del /F /Q "C:\Windows\xhunter1.sys"
+del /F /Q "C:\Windows\xhunters.log"
+
+pause
